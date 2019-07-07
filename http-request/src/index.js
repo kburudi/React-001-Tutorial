ceecddca2ed2d95d11cfd32d8d7c8a4410b607a3
zonecc <bkevery@gmail.com>
2019-07-07 11:07:40 +0300

    http requests in react

---
+++ http-request/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'Bearer TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+  return request;
+}, error => {
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  return response;
+}, error => {
+  return Promise.reject(error);
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
