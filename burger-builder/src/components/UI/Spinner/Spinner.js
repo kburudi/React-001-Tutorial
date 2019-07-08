87818bd59e4219e0f58e4b5ed1a0bdbc1dcbdeca
zonecc <bkevery@gmail.com>
2019-07-08 11:36:16 +0300

    Adding http requests to burger app

---
+++ burger-builder/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import SpinnerCss from './Spinner.css'
+
+const spinner = (props) => (
+  <div className={SpinnerCss.Loader}>Loading...</div>
+);
+
+export default spinner;
